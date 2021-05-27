33689b10ea3b8456f566c8e6c3a97d32d2e24e85
waqas837 <73779175+waqas837@users.noreply.github.com>
2021-05-27 12:06:42 +0500

    make it better

---
+++ controller/routes.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const {
+  singin,
+  signup, 
+  getData,
+  deleteUser,
+  udpateUser,
+  findSingleUser,
+  addProduct,
+  getProduct,
+  deleteProduct,findSingleProduct,udpateProduct,AdminSignIn,signupAdmin,
+  addServices,deleteServiceData,findSingleService,getserviceData,updateServiceData,
+  addtocart,getallcartSingle,findSingleProductforadd,cartSingleRemove,cartqtyUpdate,
+  findSingleCartProduct,makePayment,orders,userdataDetails,aftersalesemptycart,savePriceCalcRecord,getDataCalc
+} = require("./userLogics");
+// routes for user account
+router.post("/signup", signup);
+router.post("/signin", singin);
+router.get("/getData", getData);
+router.delete("/deleteUser/:id", deleteUser);
+router.put("/udpateUser/:id", udpateUser);
+router.get("/findSingleUser/:id", findSingleUser);
+// routes for the productPanel
+router.post("/addProduct", addProduct);
+router.get("/getProduct", getProduct);
+router.delete("/deleteProduct/:id", deleteProduct);
+router.get("/findSingleProduct/:id", findSingleProduct);
+router.put("/udpateProduct/:id", udpateProduct);
+// routes for admin account
+router.post("/signupadmin", signupAdmin);
+router.post("/signinadmin", AdminSignIn);
+// routes for the admin services 
+router.post("/addservice", addServices);
+router.get("/getServices", getserviceData);
+router.delete("/deleteservice/:id", deleteServiceData);
+router.get("/findSingleservice/:id", findSingleService);
+router.put("/udpateservice/:id", updateServiceData);
+// routes for the addtocart user
+router.post("/findSingleProductforadd/:id",findSingleProductforadd);
+router.post("/addtocartSingle",addtocart);
+router.post("/getallcartSingle",getallcartSingle);
+router.post("/cartSingleRemove/:id",cartSingleRemove);
+router.put("/cartqtyUpdate/:id",cartqtyUpdate);
+router.post("/findSingleCartProduct/:_id",findSingleCartProduct)
+// payment method
+router.post("/payment",makePayment)
+// adding after sales record for customer
+router.post("/userdataDetails",userdataDetails)
+
+// getting orders details
+router.get("/orders",orders)
+// aftersalesemptycart
+// note:we have to use the put method to delete a record as put method
+// is used for update a record but in fact we are updating our record
+// on basis of removed record
+router.put("/aftersalesemptycart",aftersalesemptycart)
+// save price calculator record
+router.post("/savePriceCalcRecord",savePriceCalcRecord)
+// get data for the price calculator
+router.get("/getDataCalc",getDataCalc)
+
+module.exports = router;
+
+
---
+++ controller/userLogics.js
@@ -0,0 +1,445 @@
+const { usersignup, Product, AdminData, ServiceData,cartadd,custmOrders,priceCalculator } = require("../Database/userSchema");
+const { v4: uuidv4 } = require("uuid");
+const stripe = require("stripe")(
+  "sk_test_51IsiGeERaO9lvsvDadobGmPv6X817aNWoxTLY1w72DPPcZMi1Ihf2mMTThB0VNP1ZaGpF0dL33GA3eNOE16zLBkb00CtlKMuR9"
+);
+const signup = async (req, res) => {
+  const data = req.body;
+  // console.log(data);
+  try {
+    if (data.password === data.cpassword) {
+      const dataCheck = new usersignup(data);
+      await dataCheck.save();
+      res.json(dataCheck.email);
+    } else {
+      res.json({ passerr: "passerr" });
+    }
+  } catch (error) {
+    console.log(`error during signup ${error}`);
+    console.log(error);
+    res.json(error);
+  }
+};
+
+//sign in data
+const singin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isExists = await usersignup.findOne({ email, password });
+    // console.log(isExists);
+    //respose
+    if (isExists === null) {
+      res.json({ err: "err" });
+    }
+    if (isExists !== null) {
+      res.json({ success: "success", user: isExists.email });
+    }
+  } catch (error) {
+    console.log(`error during sigin the data ${error}`);
+    console.log(error);
+    // res.json({err:error});
+  }
+};
+// get all data
+const getData = async (req, res) => {
+  try {
+    const data = await usersignup.find();
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during the getall data`);
+  }
+};
+//delete user
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userDelete = await usersignup.findByIdAndDelete({ _id: id });
+    // console.log(userDelete);
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//update a user
+const udpateUser = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const newData = await usersignup.findByIdAndUpdate({ _id: id }, data);
+    res.json({ data: newData });
+  } catch (error) {
+    console.log(`error during the updateUser ${error}`);
+  }
+};
+//find a single user
+const findSingleUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await usersignup.findById({ _id: id });
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during find a one user ${error}`);
+  }
+};
+// add products
+const addProduct = async (req, res) => {
+  const data = req.body;
+  // console.log(data);
+  try {
+    const dataCheck = new Product(data);
+    await dataCheck.save();
+    res.json(dataCheck.email);
+  } catch (error) {
+    console.log(`error during adding a product ${error}`);
+    console.log(error);
+    res.json(error);
+  }
+};
+
+// get all data
+const getProduct = async (req, res) => {
+  try {
+    const data = await Product.find();
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during the getall product data`);
+  }
+};
+// delete a product
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const productDelete = await Product.findByIdAndDelete({ _id: id });
+    // console.log(userDelete);
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//update the products
+const udpateProduct = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const newData = await Product.findByIdAndUpdate({ _id: id }, data);
+    res.json({ data: newData });
+  } catch (error) {
+    console.log(`error during the update products ${error}`);
+  }
+};
+//find a single product
+const findSingleProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await Product.findById({ _id: id });
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during find a one user ${error}`);
+  }
+};
+// admin singup
+const signupAdmin = async (req, res) => {
+  const data = req.body;
+  // console.log(data);
+  try {
+    if (data.password === data.cpassword) {
+      const dataCheck = new AdminData(data);
+      await dataCheck.save();
+      res.json(dataCheck.email);
+    } else {
+      res.json({ passerr: "passerr" });
+    }
+  } catch (error) {
+    console.log(`error during signup ${error}`);
+    console.log(error);
+    res.json(error);
+  }
+};
+
+//sign in data
+const AdminSignIn = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isExists = await AdminData.findOne({ email, password });
+    // console.log(isExists);
+    //respose
+    if (isExists === null) {
+      res.json({ err: "err" });
+    }
+    if (isExists !== null) {
+      res.json({ success: "success", user: isExists.email });
+    }
+  } catch (error) {
+    console.log(`error during sigin the data ${error}`);
+    console.log(error);
+    // res.json({err:error});
+  }
+};
+
+// logic for services
+
+// get all data
+const getserviceData = async (req, res) => {
+  try {
+    const data = await ServiceData.find();
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during the getall data`);
+  }
+};
+//delete user
+const deleteServiceData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userDelete = await ServiceData.findByIdAndDelete({ _id: id });
+    // console.log(userDelete);
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//update a user
+const updateServiceData = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const newData = await ServiceData.findByIdAndUpdate({ _id: id }, data);
+    res.json({ data: newData });
+  } catch (error) {
+    console.log(`error during the updateUser ${error}`);
+  }
+};
+//find a single user
+const findSingleService = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await ServiceData.findById({ _id: id });
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during find a one user ${error}`);
+  }
+};
+// add products
+const addServices = async (req, res) => {
+  const data = req.body;
+  // console.log(data);
+  try {
+    const dataCheck = new ServiceData(data);
+    await dataCheck.save();
+    res.json(dataCheck.email);
+  } catch (error) {
+    console.log(`error during adding a product ${error}`);
+    console.log(error);
+    res.json(error);
+  }
+};
+
+/////////////////////////////////
+
+//find a user and then we must only add the record
+const addtocart = async (req, res) => {
+  const data = req.body; 
+  const email ={email:data.email}
+  const title1 = data.title
+  console.log(title1)
+  try {
+    const user = await usersignup.findOne(email)
+    console.log(user)
+    
+    if(!user) return res.status(400).json({msg: "User does not exist."})
+    await usersignup.findOneAndUpdate({email: data.email}, {
+      cart: data
+  })
+    //  await cartadd.syncIndexes();
+     // if(dataResult){
+
+    // }
+    // const dataCheck =  cartadd({email:data.email,products:data});
+    // await dataCheck.save();
+     
+  } catch (error) {
+    console.log(`error during adding a product ${error}`);
+    res.json({err:"err"});
+  }
+};
+// get all the products added to cart
+const getallcartSingle = async (req, res) => {
+  const email = req.body
+     try {
+    const data = await cartadd.find({email:email.user});
+     res.json({ data:data.map((val)=>val) });
+  } catch (error) {
+    console.log(`error during the getall data ${error}`);
+  }
+};
+//remove addto cart single item
+const cartSingleRemove= async (req, res) => {
+  const { id } = req.params;
+  const email = req.body
+  try {
+    const singleUserProduct =await cartadd.deleteOne({email:email.user})
+    console.log(singleUserProduct)
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// find the single data for adding to the cart
+const findSingleProductforadd = async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  try {
+    const data = await Product.findById({ _id: id });
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during find a one product ${error}`);
+  }
+};
+//update a addtocart-qty
+const cartqtyUpdate = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  // console.log(data);
+  try {
+    const newData = await cartadd.findByIdAndUpdate({ _id: id },data);
+    res.json({ data: newData });
+  } catch (error) {
+    console.log(`error during the update cart qty ${error}`);
+  }
+};
+//find a single user
+const  findSingleCartProduct= async (req, res) => {
+  const { _id } = req.params;
+  const email = req.body;
+  try {
+    //remaining
+    const data = await cartadd.findOne({email:email.user,products:_id});
+    console.log(data);
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during find a one user's product data ${error}`);
+  }
+};
+
+//make payment
+const makePayment = (req, res) => {
+  const { totalPrice, token } = req.body;
+// console.log(totalPrice,token)
+  const idempotencyKey = uuidv4(); //so that user don't be doubled charged from the same product
+  //just in the reallity //we create stripe.customer.create({email,source:tokenid}).then((customer)=>) stripe.create a stripe.charges.create({amount:product.price*100,currency:"usd",customer:customer.id,receipt_email:token.email,description:product.name},{idempotencyKey on this transactoin}).then(result).catch(err)
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          // original account will be charged here,it recieves from the front end amount object value,for more details visit it ts file to for its object formulation
+          amount: totalPrice*100,
+          currency: "pkr",
+          customer: customer.id,
+      
+          //we can send email after a successfull transanction
+          //it is now optional just sake of fun
+          receipt_email: token.email,
+          // description: `Your product name: ${product.name}`,
+          //shipping can contain about the user's card
+          shipping: {
+            name: token.card.name,
+            address: {
+              line1:token.card.address_country,
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => {
+      //this result must contain a succedd payment data
+      res.status(200).json(result);
+     
+    })
+    .catch((err) => console.log(`error during the payment ${err}`));
+};
+
+// get all data
+const orders = async (req, res) => {
+  try {
+    const data = await custmOrders.find();
+    res.json({ data });
+  } catch (error) {
+    console.log(`error during the getall data`);
+  }
+};
+// customer after sales details
+const userdataDetails = async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  try {
+      const dataCheck = new custmOrders(data);
+      await dataCheck.save();
+  } catch (error) {
+    console.log(`error during adding customer details ${error}`);
+    console.log(error);
+    res.json(error);
+  }
+};
+
+// customer after sales details
+const aftersalesemptycart = async (req, res) => {
+  try {
+      const dataCheck =  await cartadd.deleteMany({})
+      // console.log(dataCheck)
+  } catch (error) {
+    console.log(`error during adding reomving empty cart ${error}`);
+    console.log(error);
+    res.json(error);
+  }
+};
+// savePriceCalcRecord
+const savePriceCalcRecord = async (req, res) => {
+  const data = req.body;
+  console.log(data)
+   try {
+      const dataCheck = new priceCalculator(data);
+      await dataCheck.save();
+      if(dataCheck){
+       res.json({success:true})
+     }
+  } catch (error) {
+    console.log(`error during adding price calculator ${error}`);
+    res.json(error);
+  }
+};
+// get data for user price calclutor
+const getDataCalc = async (req, res) => {
+try {
+  const data = await priceCalculator.find();
+  res.json(data)
+} catch (error) {
+  console.log(`error during getting data of price calculutor ${error}`)
+}
+}
+module.exports = {
+  signup,
+  singin,
+  getData,
+  deleteUser,
+  udpateUser,
+  findSingleUser,
+  addProduct,
+  getProduct,
+  deleteProduct,
+  udpateProduct,
+  findSingleProduct,
+  AdminSignIn,
+  signupAdmin,
+  addServices,
+  findSingleService,
+  updateServiceData,
+  deleteServiceData,getDataCalc,
+  getserviceData,addtocart,getallcartSingle,cartSingleRemove,findSingleProductforadd
+  ,cartqtyUpdate,findSingleCartProduct,makePayment,orders,userdataDetails,aftersalesemptycart,savePriceCalcRecord
+};
